1a43fa918e4dbd8b32e6c8bcf3ef762a2c252265
mgt581 <alexbryantwork3234@outlook.com>
2026-01-24 04:50:14 +0000

    new update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 @file:Suppress("DEPRECATION")
 plugins {
-    id("com.android.application")
+    alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.services)
