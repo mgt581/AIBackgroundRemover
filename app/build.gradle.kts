71673855e2ca607ecfce42634ca38cb1bab2c4ac
mgt581 <alexbryantwork3234@outlook.com>
2026-01-24 13:41:10 +0000

    upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 @file:Suppress("DEPRECATION")
 plugins {
-    
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.services)
@@ -59,15 +58,16 @@ dependencies {
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
 
-    implementation(libs.androidx.core.ktx.v1131)
+    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation("androidx.compose.ui:ui:1.10.1")
-    implementation("androidx.compose.ui:ui-graphics:1.10.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.10.1")
-    implementation("androidx.compose.material3:material3:1.4.0")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.webkit:webkit:1.15.0")
-    implementation("com.github.bumptech.glide:glide:5.0.5")
-    annotationProcessor("com.github.bumptech.glide:compiler:5.0.5")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.10.1")
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.google.material)
+    implementation(libs.androidx.webkit)
+    implementation(libs.glide)
+    annotationProcessor(libs.glide.compiler)
+    
+    debugImplementation(libs.androidx.compose.ui.tooling)
 }
