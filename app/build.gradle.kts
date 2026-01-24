f3d0dd4aac6aefb5a1ef464d18c108b07b99c1b2
mgt581 <alexbryantwork3234@outlook.com>
2026-01-24 04:54:25 +0000

    new update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     implementation(libs.play.services.auth)
 
     implementation(libs.androidx.core.ktx.v1131)
-    implementation("androidx.a:activity-compose:1.12.2")
+    implementation(libs.androidx.activity.compose)
     implementation("androidx.compose.ui:ui:1.10.1")
     implementation("androidx.compose.ui:ui-graphics:1.10.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.10.1")
