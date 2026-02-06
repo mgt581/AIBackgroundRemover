0ff13b5961295b1fea4206f19b933cab4b6c8e80
mgt581 <alexbryantwork3234@outlook.com>
2026-02-06 16:40:49 +0000

    h

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,5 @@
-@file:Suppress("DEPRECATION")
 plugins {
-    
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.services)
 }
 
@@ -13,7 +10,6 @@ android {
     defaultConfig {
         applicationId = "com.aiphotostudio.bgremover"
         minSdk = 24
-        //noinspection ExpiredTargetSdkVersion
         targetSdk = 36
         versionCode = 35
         versionName = "6.5"
@@ -32,21 +28,18 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            // This ensures debug symbols are stripped but kept for Play Console
-            ndk {
-                debugSymbolLevel = "SYMBOL_TABLE"
-            }
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     buildFeatures {
-        compose = true
-        buildConfig = true
-        resValues = false
+        viewBinding = true
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -56,34 +49,19 @@ android {
 }
 
 dependencies {
+
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
-    implementation(libs.firebase.appcheck.playintegrity)
-    implementation(libs.firebase.appcheck.debug)
-    implementation(libs.firebase.appcheck.safetynet)
     implementation(libs.play.services.auth)
-    implementation(libs.play.services.basement)
-    
-    // Modern Authentication
-    implementation(libs.androidx.credentials)
-    implementation(libs.androidx.credentials.play.services.auth)
-    implementation(libs.googleid)
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.androidx.appcompat)
     implementation(libs.google.material)
-    implementation(libs.androidx.webkit)
+
     implementation(libs.glide)
     annotationProcessor(libs.glide.compiler)
-    
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    debugImplementation(libs.androidx.compose.ui.tooling)
 }
