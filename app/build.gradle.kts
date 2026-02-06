5711d63a61b77e857ab2c54a6806b21404d07b60
mgt581 <alexbryantwork3234@outlook.com>
2026-02-06 19:20:01 +0000

    h

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,5 @@ dependencies {
     // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso-core)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
