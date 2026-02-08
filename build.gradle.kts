28ac8eb15fc8d6892a0e7199598f13dd4086edc7
mgt581 <alexbryantwork3234@outlook.com>
2026-02-08 01:44:07 +0000

    h

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,5 @@
 plugins {
-  id("com.android.application")
-  // Add the Google services Gradle plugin
-  id("com.google.gms.google-services")
-}
-
-android {
-    namespace = "com.example.aiphotostudio"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.aiphotostudio"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-}
-
-dependencies {
-  // Import the Firebase BoM
-  implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
-
-  // TODO: Add the dependencies for Firebase products you want to use
-  // When using the BoM, don't specify versions in Firebase dependencies
-  implementation("com.google.firebase:firebase-analytics")
-
-  // Add the dependencies for any other desired Firebase products
-  // https://firebase.google.com/docs/android/setup#available-libraries
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.google.services) apply false
 }
