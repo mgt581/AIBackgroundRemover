c2cfaf9a0733e16d751402ed72a3e0b30b3f615a
mgt581 <alexbryantwork3234@outlook.com>
2026-02-12 00:15:58 +0000

    trying to get fine line small squares checkerboard to replace big squares

---
+++ app/src/main/java/com/aiphotostudio/bgremover/AIApplication.kt
@@ -0,0 +1,51 @@
+package com.aiphotostudio.bgremover
+
+import android.app.Application
+import android.util.Log
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+
+class AIApplication : Application() {
+    
+    override fun onCreate() {
+        super.onCreate()
+        
+        try {
+            // Initialize Firebase
+            FirebaseApp.initializeApp(this)
+            
+            val appCheck = FirebaseAppCheck.getInstance()
+
+            // Generated BuildConfig will be available after a successful build
+            if (BuildConfig.DEBUG) {
+                Log.d("AIApplication", "App Check: Installing DebugAppCheckProviderFactory")
+                appCheck.installAppCheckProviderFactory(
+                    DebugAppCheckProviderFactory.getInstance()
+                )
+            } else {
+                val playServicesStatus = GoogleApiAvailability.getInstance()
+                    .isGooglePlayServicesAvailable(this)
+                if (playServicesStatus == ConnectionResult.SUCCESS) {
+                    Log.d("AIApplication", "App Check: Installing PlayIntegrityAppCheckProviderFactory")
+                    appCheck.installAppCheckProviderFactory(
+                        PlayIntegrityAppCheckProviderFactory.getInstance()
+                    )
+                } else {
+                    Log.w(
+                        "AIApplication",
+                        "App Check disabled: Play Services unavailable"
+                    )
+                }
+            }
+            appCheck.setTokenAutoRefreshEnabled(true)
+
+            Log.d("AIApplication", "Firebase and App Check initialized successfully")
+        } catch (e: Exception) {
+            Log.e("AIApplication", "Firebase initialization failed", e)
+        }
+    }
+}
