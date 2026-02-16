b48a3003f2d52f59a49de20bd54c2d8df1d0da4b
mgt581 <alexbryantwork3234@outlook.com>
2026-02-16 14:07:05 +0000

    this is the best working set up version as far only thing web view buttons not working and sort header we added a colour picker chart for choose bg button This reverts commit a07c595f

--- app/src/main/java/com/aiphotostudio/bgremover/MainActivity.kt
+++ app/src/main/java/com/aiphotostudio/bgremover/MainActivity.kt
@@ -237,7 +237,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             
-            loadUrl("https://aiphotostudio.co.uk")
+            loadUrl("https://aiphotostudio.co")
         }
     }
 
